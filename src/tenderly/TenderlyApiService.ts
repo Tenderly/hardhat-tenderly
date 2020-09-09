871539769873a6987dae457c3464c9519c4a6de4
viktor <viktor@tenderly.co>
2020-09-09 15:51:51 +0200

    Extract api loginc to service

---
+++ src/tenderly/TenderlyApiService.ts
@@ -0,0 +1,32 @@
+import {AxiosInstance, AxiosRequestConfig} from "axios";
+import axios from "axios"
+import {TenderlyConfig} from "./types";
+import {homedir} from "os";
+import fs from "fs";
+import * as yaml from "js-yaml";
+import {BuidlerPluginError} from "@nomiclabs/buidler/plugins";
+import {PluginName} from "../index";
+import {TENDERLY_API_BASE_URL, TENDERLY_DASHBOARD_BASE_URL} from "./TenderlyService";
+
+export class TenderlyApiService {
+  public static configureInstance(): AxiosInstance {
+    const yamlData = this.getTenderlyConfig()
+    return axios.create({
+      baseURL: TENDERLY_API_BASE_URL,
+      headers: {"x-access-key": yamlData.access_key}
+    })
+  }
+
+  private static getTenderlyConfig(): TenderlyConfig {
+    const filepath = homedir() + "/.tenderly/config.yaml"
+    const fileData = fs.readFileSync(filepath)
+    const yamlData: TenderlyConfig = yaml.load(fileData.toString())
+
+    if (yamlData.access_key == null) {
+      throw new BuidlerPluginError(PluginName, `Access token not provided at filepath ${filepath}.\n` +
+        `You can find the token at ${TENDERLY_DASHBOARD_BASE_URL}/account/authorization`)
+    }
+
+    return yamlData
+  }
+}
--- src/tenderly/TenderlyService.ts
+++ src/tenderly/TenderlyService.ts
@@ -5,23 +5,19 @@ import {homedir} from "os";
 import * as yaml from "js-yaml"
 import fs from "fs"
 import {PluginName} from "../index"
+import {TenderlyApiService} from "./TenderlyApiService";
 
-const TENDERLY_API_BASE_URL = "https://api.tenderly.co"
-const TENDERLY_DASHBOARD_BASE_URL = "https://dashboard.tenderly.co"
+export const TENDERLY_API_BASE_URL = "https://api.tenderly.co"
+export const TENDERLY_DASHBOARD_BASE_URL = "https://dashboard.tenderly.co"
 
 export class TenderlyService {
   public static async verifyContracts(
     request: TenderlyContractUploadRequest
   ) {
-    const yamlData = this.getTenderlyConfig()
-
-    const tenderlyApiInstance = axios.create({
-      baseURL: TENDERLY_API_BASE_URL,
-      headers: {"x-access-key": yamlData.access_key}
-    })
+    const tenderlyApi = TenderlyApiService.configureInstance();
 
     try {
-      const response = await tenderlyApiInstance.post(
+      const response = await tenderlyApi.post(
         "/api/v1/account/me/verify-contracts",
         {...request}
       )
@@ -38,15 +34,10 @@ export class TenderlyService {
     projectSlug: string,
     username: string,
   ) {
-    const yamlData = this.getTenderlyConfig()
-
-    const tenderlyApiInstance = axios.create({
-      baseURL: TENDERLY_API_BASE_URL,
-      headers: {"x-access-key": yamlData.access_key}
-    })
+    const tenderlyApi = TenderlyApiService.configureInstance();
 
     try {
-      const response = await tenderlyApiInstance.post(
+      const response = await tenderlyApi.post(
         `/api/v1/account/${username}/project/${projectSlug}/contracts`,
         {...request}
       )
@@ -58,18 +49,5 @@ export class TenderlyService {
       console.log(`Error in ${PluginName}: There was an error during the request. Contract push failed`)
       console.log(error.response)
     }
-  }
-
-  private static getTenderlyConfig(): TenderlyConfig {
-    const filepath = homedir() + "/.tenderly/config.yaml"
-    const fileData = fs.readFileSync(filepath)
-    const yamlData: TenderlyConfig = yaml.load(fileData.toString())
-
-    if (yamlData.access_key == null) {
-      throw new BuidlerPluginError(PluginName, `Access token not provided at filepath ${filepath}.\n` +
-        `You can find the token at ${TENDERLY_DASHBOARD_BASE_URL}/account/authorization`)
-    }
-
-    return yamlData
   }
 }
