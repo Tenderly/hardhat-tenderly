bb70a7e1bbb85175cceac4ee5d42c4ac21fc5880
viktor <viktor@tenderly.co>
2020-10-28 16:30:28 +0100

    update rpc endpoint

--- src/tenderly/TenderlyService.ts
+++ src/tenderly/TenderlyService.ts
@@ -11,7 +11,7 @@ import {
 export const TENDERLY_API_BASE_URL = "https://api.tenderly.co";
 export const TENDERLY_DASHBOARD_BASE_URL = "https://dashboard.tenderly.co";
 // TODO(viktor): change this when we release rpc
-export const TENDERLY_RPC_BASE = "http://127.0.0.1:8545/rpc";
+export const TENDERLY_RPC_BASE = "http://127.0.0.1:8545";
 
 export class TenderlyService {
   public static async verifyContracts(request: TenderlyContractUploadRequest) {
