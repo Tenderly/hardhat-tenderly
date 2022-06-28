4ddf11b3081eaa0369bbdd46b57fc42aa30dcd39
Filip Petrović <filippetroviccc@users.noreply.github.com>
2022-06-28 15:14:38 +0200

    Fork verification API (#50)
    * ability to turn off automatic verification and verifyForkAPI more accessible
    
    * removed chai
    
    * small improvments

--- src/tenderly/errors.ts
+++ src/tenderly/errors.ts
@@ -5,4 +5,4 @@ export const NO_VERIFIABLE_CONTRACTS_ERROR = `${Plugin}: Contracts are not eligi
 export const NO_NEW_CONTRACTS_VERIFIED_ERROR = `${Plugin}: Warning: No new contracts have been verified. \n  Contract not eligible for verification %s`;
 export const NO_COMPILER_FOUND_FOR_CONTRACT = `Error in ${Plugin}: No compiler configuration found for the contracts`;
 export const API_VERIFICATION_REQUEST_ERROR = `Error in ${Plugin}: Verification failed. There was an error during the API request, please contact support with the above error.`;
-export const CONTRACTS_NOT_DETECTED = `"Could not detect any contracts inside hardhat project. Make sure you have some contracts under ./contracts directory."`;
+export const CONTRACTS_NOT_DETECTED = `Could not detect any contracts inside hardhat project. Make sure you have some contracts under ./contracts directory.`;
