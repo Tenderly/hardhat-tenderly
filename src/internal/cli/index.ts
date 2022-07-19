f55bd62f0c00c3f1b808721227d4b1adaaefdbff
Igor Petkovic <igorpetkovic@gmail.com>
2022-07-19 14:43:16 +0200

    implement npx login command

--- src/internal/cli/index.ts
+++ src/internal/cli/index.ts
@@ -1,10 +1,12 @@
 #! /usr/bin/env node
 import { Command } from "commander";
 import { NetworksCommand } from "./commands";
+import { LoginCommand } from "./commands";
 
 (async () => {
   try {
     const program = new Command();
+    program.addCommand(LoginCommand);
     program.addCommand(NetworksCommand);
 
     await program.parseAsync(process.argv);
---
+++ src/utils/config.ts
@@ -0,0 +1,55 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+import * as yaml from "js-yaml";
+
+const filepath = os.homedir() + path.sep + ".tenderly" + path.sep + "config.yaml";
+
+export function initConfig() {
+    const yamlData = {
+        access_key: "",
+        access_key_id: "",
+        account_id: "",
+        email: "",
+        token: "",
+        username: ""
+    };
+
+    fs.writeFileSync(filepath, yaml.safeDump(yamlData), "utf8");
+}
+
+export function getConfig() {
+    const fileData = fs.readFileSync(filepath);
+    const yamlData = yaml.load(fileData.toString());
+
+    return yamlData;
+}
+
+export function configExists(): boolean {
+    return fs.existsSync(filepath);
+}
+
+export function isAccessTokenSet(): boolean {
+    if (!configExists()) {
+        return false;
+    }
+
+    const accessToken = getConfig().access_key
+
+    return accessToken != undefined && accessToken != "";
+}
+
+export function getAccessToken(): string {
+    if (!isAccessTokenSet()) {
+        return "";
+    }
+
+    return getConfig().access_key;
+}
+
+export function setAccessToken(accessToken: string) {
+    const yamlData = getConfig();
+    yamlData.access_key = accessToken;
+
+    fs.writeFileSync(filepath, yaml.safeDump(yamlData), "utf8");
+}
