0a54656c9300dc79fa45b72bb03bee5776bd884d
Filip Petrovic <filip.petroviccc@gmail.com>
2022-08-03 13:57:52 +0200

    adding support for deployTransaction

--- src/tenderly/ethers/Contract.ts
+++ src/tenderly/ethers/Contract.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import { Contract, ethers } from "ethers";
 import { Libraries } from "hardhat-deploy/dist/types";
 
@@ -28,18 +29,33 @@ export class TdlyContract {
         return;
       }
 
+      if (key === "deployTransaction") {
+        const wait = nativeContract[key].wait;
+
+        nativeContract[key].wait = async (
+          confirmations?: number | undefined
+        ): Promise<TransactionReceipt> => {
+          const receipt = await wait(confirmations);
+          await this.tdlyVerify(receipt.contractAddress);
+
+          return receipt;
+        };
+      }
+
       this[key] = nativeContract[key];
     });
   }
 
   public async deployed(): Promise<Contract> {
     const contract = await this.nativeContract.deployed();
+    await this.tdlyVerify(contract.address);
+    return contract;
+  }
 
-    // if contract is library skip verification
-
+  private async tdlyVerify(address: string) {
     const contPair: ContractByName = {
       name: this.contractName,
-      address: contract.address
+      address
     };
     if (this.libraries) {
       contPair.libraries = this.libraries;
@@ -47,6 +63,41 @@ export class TdlyContract {
 
     await this.tenderly.persistArtifacts(contPair);
     await this.tenderly.verify(contPair);
-    return contract;
   }
+}
+
+export interface TransactionReceipt {
+  to: string;
+  from: string;
+  contractAddress: string;
+  transactionIndex: number;
+  root?: string;
+  gasUsed: BigNumber;
+  logsBloom: string;
+  blockHash: string;
+  transactionHash: string;
+  logs: Log[];
+  blockNumber: number;
+  confirmations: number;
+  cumulativeGasUsed: BigNumber;
+  effectiveGasPrice: BigNumber;
+  byzantium: boolean;
+  type: number;
+  status?: number;
+}
+
+export interface Log {
+  blockNumber: number;
+  blockHash: string;
+  transactionIndex: number;
+
+  removed: boolean;
+
+  address: string;
+  data: string;
+
+  topics: string[];
+
+  transactionHash: string;
+  logIndex: number;
 }
