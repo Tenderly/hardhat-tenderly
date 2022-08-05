4c60114e0995d490ef7174bec9e35a4e2a2f7829
Igor Petkovic <igorpetkovic@gmail.com>
2022-08-05 16:08:17 +0200

    display link to dash; read headers

--- src/internal/cli/commands/vnetServer.ts
+++ src/internal/cli/commands/vnetServer.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import got from "got";
+import supportsHyperlinks from "supports-hyperlinks";
+import hyperlinker from "hyperlinker";
 
 const app = express();
 app.use(express.json());
@@ -7,22 +9,34 @@ app.use(express.json());
 app.use(async (req, res) => {
   console.log(req.body.method);
 
-  const tdlyRes: any = await got
-    .post("https://rpc.tenderly.co/fork/ed6f31c5-9d52-46b3-9181-54b8970e12cc", {
+  const response: any = await got.post(
+    "https://rpc.tenderly.co/fork/e2467cfc-1a6a-4e01-b365-3a0406c9e0e2",
+    {
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(req.body),
-    })
-    .json();
+    }
+  );
 
-  // if (tdlyRes?.result?.hash) {
-  //   console.log(
-  //     "https://dashboard.tenderly.co/Riphal/project1/fork/ed6f31c5-9d52-46b3-9181-54b8970e12cc/simulation/" +
-  //       tdlyRes.result
-  //   );
-  //   console.log(tdlyRes);
-  // }
+  const tdlyRes = JSON.parse(response.body);
+
+  if (tdlyRes?.result?.hash) {
+    if (supportsHyperlinks.stdout) {
+      console.log(
+        hyperlinker(
+          "View in Tenderly",
+          "https://dashboard.tenderly.co/igorpetkovic/project/fork/e2467cfc-1a6a-4e01-b365-3a0406c9e0e2/simulation/" +
+            response.headers["head"]
+        )
+      );
+    } else {
+      console.log(
+        "https://dashboard.tenderly.co/igorpetkovic/project/fork/e2467cfc-1a6a-4e01-b365-3a0406c9e0e2/simulation/" +
+          response.headers["head"]
+      );
+    }
+  }
 
   res.send(tdlyRes);
 });
