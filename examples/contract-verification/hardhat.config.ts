65ab1287711e8cd0d5b85146f7f9a4bca7de459e
Boris Jankovic <boris@tenderly.co>
2022-09-21 12:37:56 +0200

    Changes

--- examples/contract-verification/hardhat.config.ts
+++ examples/contract-verification/hardhat.config.ts
@@ -4,26 +4,14 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const {
-  TENDERLY_AUTOMATIC_VERIFICATION,
-  TENDERLY_PRIVATE_VERIFICATION,
-  TENDERLY_FORK_ID,
-} = process.env;
+const { TENDERLY_AUTOMATIC_VERIFICATION, TENDERLY_PRIVATE_VERIFICATION, TENDERLY_FORK_ID } = process.env;
 
 const automaticVerification = TENDERLY_AUTOMATIC_VERIFICATION === "true";
 const priaveteVerification = TENDERLY_PRIVATE_VERIFICATION === "true";
 
-console.log(
-  "Using automatic verification? ",
-  automaticVerification,
-  TENDERLY_AUTOMATIC_VERIFICATION
-);
+console.log("Using automatic verification? ", automaticVerification, TENDERLY_AUTOMATIC_VERIFICATION);
 
-console.log(
-  "Using private verification? ",
-  priaveteVerification,
-  TENDERLY_PRIVATE_VERIFICATION
-);
+console.log("Using private verification? ", priaveteVerification, TENDERLY_PRIVATE_VERIFICATION);
 
 tdly.setup({ automaticVerifications: automaticVerification });
 
@@ -33,12 +21,11 @@ const config: HardhatUserConfig = {
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL,
-      accounts: !!process.env.ROPSTEN_PRIVATE_KEY
-        ? [process.env.ROPSTEN_PRIVATE_KEY]
-        : [],
+      accounts: !!process.env.ROPSTEN_PRIVATE_KEY ? [process.env.ROPSTEN_PRIVATE_KEY] : [],
     },
     tenderly: {
-      url: `http://127.0.0.1:1337`,
+      url: `https://rpc.tenderly.co/fork/${TENDERLY_FORK_ID}`,
+      // url: `http://127.0.0.1:1337`,
     },
   },
   tenderly: {
