f0d42e84c6bdd92c024d39923fc0fe189c5e0aa1
Boris Janković <39570184+Riphal@users.noreply.github.com>
2022-10-31 15:56:27 +0100

    Fix ethers override (#80)

--- packages/tenderly-hardhat/src/index.ts
+++ packages/tenderly-hardhat/src/index.ts
@@ -1,8 +1,10 @@
 import "@nomiclabs/hardhat-ethers";
-import "./tenderly/extender";
 import "./type-extensions";
 import "./tasks";
 
+import * as tenderlyExtender from "./tenderly/extender";
+
 export function setup({ automaticVerifications = false }): void {
   process.env.AUTOMATIC_VERIFICATION_ENABLED = automaticVerifications === true ? "true" : "false";
+  tenderlyExtender.setup();
 }
