bb2c185d27351d627c2aba809415cc890db5cad9
dule-git <dusank98@gmail.com>
2022-11-24 13:33:27 +0100

    Changed minLogLevel to be a optional value that is set to 7 by default

--- packages/tenderly-hardhat/src/index.ts
+++ packages/tenderly-hardhat/src/index.ts
@@ -4,7 +4,9 @@ import "./tasks";
 
 import * as tenderlyExtender from "./tenderly/extender";
 
-export function setup(cfg = { automaticVerifications: false, minLogLevel: 7 }): void {
+export function setup(
+  cfg: { automaticVerifications: boolean; minLogLevel?: number } = { automaticVerifications: false, minLogLevel: 7 }
+): void {
   process.env.AUTOMATIC_VERIFICATION_ENABLED = cfg.automaticVerifications === true ? "true" : "false";
   process.env.MIN_LOG_LEVEL = cfg.minLogLevel !== undefined ? cfg.minLogLevel.toString() : "7";
 
