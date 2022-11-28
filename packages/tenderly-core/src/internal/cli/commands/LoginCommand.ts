f50b6044adbc8f8fc79063cee6d38d64ef19dbce
dule-git <dusank98@gmail.com>
2022-11-28 12:58:26 +0100

    Added logs to LoginCommand.ts

--- packages/tenderly-core/src/internal/cli/commands/LoginCommand.ts
+++ packages/tenderly-core/src/internal/cli/commands/LoginCommand.ts
@@ -2,32 +2,41 @@ import open from "open";
 import axios from "axios";
 import prompts from "prompts";
 import commander from "commander";
+import { logger } from "../../../utils/logger";
 
 import { isAccessTokenSet, setAccessToken } from "../../../utils/config";
 import { TENDERLY_API_BASE_URL, TENDERLY_DASHBOARD_BASE_URL } from "../../../common/constants";
 
 export const LoginCommand = new commander.Command("login").description("login to Tenderly").action(async () => {
+  logger.info("Making LoginCommand...");
+
   if (isAccessTokenSet()) {
+    logger.debug("Access token is set. Prompting user to overwrite access token with a new one.");
     const response = await prompts({
       type: "confirm",
       name: "overwrite",
       message: "Access token already set. Would you like to overwrite it?",
     });
     if (!response.overwrite) {
+      logger.debug("User didn't request an overwrite of the token. Login command is made with existing token.");
       return;
     }
   }
 
+  logger.info("Access token isn't set. Prompting user to enter access token...");
   const accessToken = await promptAccessToken();
 
+  logger.debug("User entered access token. Setting access token...");
   setAccessToken(accessToken);
-  console.log("Successfully logged in.");
+
+  logger.info("Successfully logged in.");
 });
 
 async function promptAccessToken(): Promise<string> {
-  console.log(`Redirecting to ${TENDERLY_DASHBOARD_BASE_URL}/account/authorization`);
+  logger.debug(`Redirecting to ${TENDERLY_DASHBOARD_BASE_URL}/account/authorization`);
   await open(`${TENDERLY_DASHBOARD_BASE_URL}/account/authorization`);
 
+  logger.info("Prompting user to enter access token");
   const response = await prompts({
     type: "text",
     name: "accessToken",
@@ -35,6 +44,7 @@ async function promptAccessToken(): Promise<string> {
     validate: validator,
   });
 
+  logger.info("User entered access token:", response.accessToken);
   return response.accessToken;
 }
 
@@ -53,14 +63,20 @@ const validator = async function (value: string) {
 
 async function canAuthenticate(accessToken: string): Promise<boolean> {
   try {
+    logger.debug("Determining if the user can be authenticated with provided access token:", accessToken);
+
+    logger.trace("Making a call to the url...");
     const response = await axios.get(`${TENDERLY_API_BASE_URL}/api/v1/user`, {
       headers: { "x-access-key": accessToken },
     });
     if (response.data.user !== undefined) {
+      logger.debug("The user can be authenticated.");
       return true;
     }
+    logger.error("The user cannot be authenticated with access token:", accessToken);
     return false;
   } catch (err) {
+    logger.error(`There was an error while making the api call ${TENDERLY_API_BASE_URL}/api/v1/user`);
     return false;
   }
 }
