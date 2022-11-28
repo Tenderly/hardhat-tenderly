321d7276a408f3566549846e01504b01a171d86b
dule-git <dusank98@gmail.com>
2022-11-28 12:35:14 +0100

    Added logs to config.ts

--- packages/tenderly-core/src/utils/config.ts
+++ packages/tenderly-core/src/utils/config.ts
@@ -2,17 +2,25 @@ import fs from "fs";
 import os from "os";
 import path from "path";
 import * as yaml from "js-yaml";
-
 import { TenderlyConfig } from "../types";
+import { logger } from "./logger";
 
 const configDir = `${os.homedir() + path.sep}.tenderly`;
 export const configFilePath = `${configDir + path.sep}config.yaml`;
 
 export function getConfig(): TenderlyConfig {
+  logger.trace("Getting tenderly config...");
+
   if (configExists()) {
     const fileData = fs.readFileSync(configFilePath);
-    return yaml.load(fileData.toString()) as TenderlyConfig;
+
+    const tenderlyConfig = yaml.load(fileData.toString()) as TenderlyConfig;
+    logger.trace("Tenderly config exists. Value of the config:", tenderlyConfig);
+
+    return tenderlyConfig;
   }
+
+  logger.trace("Tenderly config doesn't exist, empty string values have been returned.");
   return {
     access_key: "",
     access_key_id: "",
@@ -24,27 +32,43 @@ export function getConfig(): TenderlyConfig {
 }
 
 export function writeConfig(config: TenderlyConfig): void {
+  logger.trace(`Writing config to a file @ ${configDir}/${configFilePath}`);
+  logger.trace("Value of the config:", config);
+
   fs.mkdirSync(configDir, { recursive: true });
   fs.writeFileSync(configFilePath, yaml.dump(config), "utf8");
 }
 
 export function configExists(): boolean {
+  logger.trace("Checking if config exists...");
   return fs.existsSync(configFilePath);
 }
 
 export function isAccessTokenSet(): boolean {
+  logger.trace("Determining if access token in tenderly config file is set...");
   const config = getConfig();
-  return config.access_key !== undefined || config.access_key !== "";
+
+  const isSet = config.access_key !== undefined || config.access_key !== "";
+  logger.trace("Access key is set:", isSet);
+
+  return isSet;
 }
 
 export function getAccessToken(): string {
+  logger.trace("Getting access token...");
   if (!isAccessTokenSet()) {
+    logger.trace("Access key is not set, returning empty string value.");
     return "";
   }
-  return getConfig().access_key;
+
+  const accessKey = getConfig().access_key;
+  logger.trace("Access key:", accessKey);
+
+  return accessKey;
 }
 
 export function setAccessToken(accessToken: string): void {
+  logger.trace("Setting access key to value:", accessToken, " ....");
   const config = getConfig();
   config.access_key = accessToken;
   writeConfig(config);
