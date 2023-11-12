9c5c01f846b7ced825d2851bfcab7d633233fe0a
dule-git <dusank98@gmail.com>
2023-11-12 19:33:05 +0100

    add automaticVerifications: true as a default option

--- packages/tenderly-hardhat/src/index.ts
+++ packages/tenderly-hardhat/src/index.ts
@@ -3,7 +3,7 @@ import "./tasks";
 
 import * as tenderlyExtender from "./tenderly/extender";
 
-export function setup(cfg: { automaticVerifications: boolean } = { automaticVerifications: false }): void {
+export function setup(cfg: { automaticVerifications: boolean } = { automaticVerifications: true }): void {
   process.env.AUTOMATIC_VERIFICATION_ENABLED = cfg.automaticVerifications === true ? "true" : "false";
   tenderlyExtender.setup();
 }
