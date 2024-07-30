281a413ad965c6cc4c566ccabf07245febf759d5
Jelena Arsenijevic <jelena.arsenijevic@tenderly.co>
2024-07-30 13:18:28 +0200

    call tenderly verification

--- packages/tenderly-hardhat/src/tasks/ignition-deploy-task.ts
+++ packages/tenderly-hardhat/src/tasks/ignition-deploy-task.ts
@@ -3,13 +3,7 @@ import {logger} from "../utils/logger";
 import path from "path";
 import {HardhatPluginError} from "hardhat/plugins";
 import {PLUGIN_NAME} from "../constants";
-import {getAccessToken} from "tenderly/utils/config";
-
-import {getVnetTypeByEndpointId, VnetType} from "../tenderly/vnet-type";
-import * as URLComposer from "../utils/url-composer";
-import {getVerificationInformation, IgnitionError, VerifyInfo} from "@nomicfoundation/ignition-core";
-import {getChainId} from "../tenderly/extender";
-import {Etherscan} from "@nomicfoundation/hardhat-verify/etherscan";
+import {getVerificationInformation, IgnitionError} from "@nomicfoundation/ignition-core";
 import {ChainConfig} from "@nomicfoundation/hardhat-verify/types";
 
 require("@nomicfoundation/ignition-core");
@@ -30,18 +24,18 @@ ignitionScope
     async (
       {
         deploymentId,
+        includeUnrelatedContracts,
       },
       hre,
       runSuper
     ) => {
-      const shouldVerify = process.env.AUTOMATIC_VERIFICATION_ENABLED;
-      if (shouldVerify === "true") {
-        logger.debug("Automatic verification is enabled, extending ignition deploy task.");
-      }
       await runSuper();
 
+      const shouldVerify = process.env.AUTOMATIC_VERIFICATION_ENABLED;
       if (!shouldVerify)
         return;
+
+      logger.debug("Automatic verification is enabled, extending ignition deploy task.");
 
       if (deploymentId === undefined || deploymentId === "") {
         throw new HardhatPluginError(
@@ -57,43 +51,7 @@ ignitionScope
         deploymentId
       );
 
-      const accessKey = getAccessToken();
-      if (accessKey === "") {
-        throw new HardhatPluginError(
-          PLUGIN_NAME,
-          "Tenderly access key is not set. Please set TENDERLY_ACCESS_KEY environment variable.",
-        );
-      }
-
-      const chainId = await getChainId(hre.network);
-
-      // @ts-ignore
-      const endpointId = hre.network.config.url.split("/").pop();
-      if (endpointId === undefined) {
-        throw new Error(
-          "Could not locate the UUID at the end of a Tenderly RPC URL.",
-        );
-      }
-
-      const vnetType = await getVnetTypeByEndpointId(hre, endpointId);
-      if (vnetType === VnetType.NULL_TYPE) {
-        throw new Error("Couldn't recognize VnetType from endpoint id.");
-      }
-
-      const customChainConfigs = [{
-        network: hre.network.name,
-        chainId,
-        urls: {
-          apiURL: URLComposer.composeApiURL(hre, endpointId, chainId, vnetType),
-          browserURL: URLComposer.composeBrowserURL(
-            hre,
-            endpointId,
-            chainId,
-            vnetType,
-          ),
-        },
-      }]
-
+      const customChainConfigs: ChainConfig[] = []
       try {
         for await (const [
           chainConfig,
@@ -101,42 +59,14 @@ ignitionScope
         ] of getVerificationInformation(
           deploymentDir,
           customChainConfigs,
-          false
+          includeUnrelatedContracts,
         )) {
-          // @ts-ignore
-          const apiKeyAndUrls = getApiKeyAndUrls(
-            accessKey,
-            chainConfig
-          );
 
-          const instance = new Etherscan(...apiKeyAndUrls);
+          await hre.tenderly.verify({
+            name: contractInfo.name,
+            address: contractInfo.address,
+          });
 
-          console.log(
-            `Verifying contract "${contractInfo.name}" for network ${chainConfig.network}...`
-          );
-
-          const result = await verifyEtherscanContract(instance, contractInfo);
-
-          if (result.type === "success") {
-            console.log(
-              `Successfully verified contract "${contractInfo.name}" for network ${chainConfig.network}:\n  - ${result.contractURL}`
-            );
-            console.log("");
-          } else {
-            if (/already verified/gi.test(result.reason.message)) {
-              const contractURL = instance.getContractUrl(contractInfo.address);
-              console.log(
-                `Contract ${contractInfo.name} already verified on network ${chainConfig.network}:\n  - ${contractURL}`
-              );
-              console.log("");
-
-            } else {
-              throw new HardhatPluginError(
-                PLUGIN_NAME,
-                `Verification failed. Please run \`hardhat ignition verify ${deploymentId} --include-unrelated-contracts\` to attempt verifying all contracts.`
-              );
-            }
-          }
         }
       } catch (e) {
         if (e instanceof IgnitionError) {
@@ -151,56 +81,3 @@ ignitionScope
       }
     }
   );
-
-function getApiKeyAndUrls(
-  etherscanApiKey: string | Record<string, string>,
-  chainConfig: ChainConfig
-): [apiKey: string, apiUrl: string, webUrl: string] {
-  const apiKey: string =
-    typeof etherscanApiKey === "string"
-      ? etherscanApiKey
-      : etherscanApiKey[chainConfig.network];
-
-  if (apiKey === undefined) {
-    throw new HardhatPluginError(
-      PLUGIN_NAME,
-      `No etherscan API key configured for network ${chainConfig.network}`
-    );
-  }
-
-  return [apiKey, chainConfig.urls.apiURL, chainConfig.urls.browserURL];
-}
-
-async function verifyEtherscanContract(
-  etherscanInstance: Etherscan,
-  { address, compilerVersion, sourceCode, name, args }: VerifyInfo
-): Promise<
-  { type: "success"; contractURL: string } | { type: "failure"; reason: Error }
-> {
-  try {
-    const { message: guid } = await etherscanInstance.verify(
-      address,
-      sourceCode,
-      name,
-      compilerVersion,
-      args
-    );
-
-    const verificationStatus = await etherscanInstance.getVerificationStatus(
-      guid
-    );
-
-    if (verificationStatus.isSuccess()) {
-      const contractURL = etherscanInstance.getContractUrl(address);
-      return { type: "success", contractURL };
-    } else {
-      return { type: "failure", reason: new Error(verificationStatus.message) };
-    }
-  } catch (e) {
-    if (e instanceof Error) {
-      return { type: "failure", reason: e };
-    } else {
-      throw e;
-    }
-  }
-}
