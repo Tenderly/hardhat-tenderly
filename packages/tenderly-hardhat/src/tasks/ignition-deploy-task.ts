81d05944900fa16e42c3a786514b8afcd38ac82c
Jelena Arsenijević <jelenarsnjvc@gmail.com>
2024-07-31 15:07:43 +0200

    [SOP-119] Support hardhat-ignition (#191)
    * support hardhat ignition
    
    * call tenderly verification
    
    * calling tenderly verification
    
    * Update packages/tenderly-hardhat/package.json
    
    * support hardhat-ignition

---
+++ packages/tenderly-hardhat/src/tasks/ignition-deploy-task.ts
@@ -0,0 +1,83 @@
+import {scope} from "hardhat/config";
+import {logger} from "../utils/logger";
+import path from "path";
+import {HardhatPluginError} from "hardhat/plugins";
+import {PLUGIN_NAME} from "../constants";
+import {getVerificationInformation, IgnitionError} from "@nomicfoundation/ignition-core";
+import {ChainConfig} from "@nomicfoundation/hardhat-verify/types";
+
+require("@nomicfoundation/ignition-core");
+require("fs-extra");
+require("hardhat/config");
+require("hardhat/plugins");
+require("path");
+
+
+const ignitionScope = scope(
+  "ignition",
+  "Deploy your smart contracts using Hardhat Ignition"
+);
+
+ignitionScope
+  .task("deploy")
+  .setAction(
+    async (
+      {
+        deploymentId,
+        includeUnrelatedContracts,
+      },
+      hre,
+      runSuper
+    ) => {
+      await runSuper();
+
+      const shouldVerify = process.env.AUTOMATIC_VERIFICATION_ENABLED;
+      if (!shouldVerify)
+        return;
+
+      logger.debug("Automatic verification is enabled, extending ignition deploy task.");
+
+      if (deploymentId === undefined || deploymentId === "") {
+        throw new HardhatPluginError(
+          PLUGIN_NAME,
+          "No deployment ID provided, exiting."
+        );
+      }
+
+      const deploymentDir = path.join(
+        // @ts-ignore
+        hre.config.paths.ignition,
+        "deployments",
+        deploymentId
+      );
+
+      const customChainConfigs: ChainConfig[] = []
+      try {
+        for await (const [
+          chainConfig,
+          contractInfo,
+        ] of getVerificationInformation(
+          deploymentDir,
+          customChainConfigs,
+          includeUnrelatedContracts,
+        )) {
+
+          await hre.tenderly.verify({
+            name: contractInfo.name,
+            address: contractInfo.address,
+          });
+
+        }
+      } catch (e) {
+        if (e instanceof IgnitionError) {
+          throw new HardhatPluginError(
+            PLUGIN_NAME,
+            e.message,
+            e
+          );
+        }
+
+        throw e;
+      }
+    }
+  );
