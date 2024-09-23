cb559a5cec7c0d1afa86112274fc5539f0b047a3
dule-git <61541725+dule-git@users.noreply.github.com>
2024-09-23 22:00:17 +0200

    Initial hardhat ignition example (#211)
    * script for version release
    
    * initional hardhat-ignition example
    
    * added hardhat-tenderly to ignition project

---
+++ examples/contract-verification/hardhat-ignition/contracts/Calculator.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./libraries/Maths.sol";
+
+library Calculator {
+    using Maths for uint32;
+
+    function calculateStuff(uint32 startingPoint)
+        public
+        pure
+        returns (uint256, uint256)
+    {
+        return (
+            startingPoint.sumOfArithmeticProgression(2, 4),
+            startingPoint.sumOfGeometricProgression(2, 4)
+        );
+    }
+}
