77ffff137c0938bdef9caef7240bbd9e2467d2e3
dule-git <61541725+dule-git@users.noreply.github.com>
2024-09-26 15:01:30 +0200

    modify examples to not use setup function

--- examples/contract-verification/hardhat-ignition/hardhat.config.ts
+++ examples/contract-verification/hardhat-ignition/hardhat.config.ts
@@ -2,11 +2,9 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers";
 import * as dotenv from "dotenv";
-import * as tdly from "@tenderly/hardhat-tenderly";
+import "@tenderly/hardhat-tenderly";
 
 dotenv.config();
-
-tdly.setup();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
